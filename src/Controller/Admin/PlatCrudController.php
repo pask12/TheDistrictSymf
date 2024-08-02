c3c4003ae139849095891f7c54ecbab39870bbcf
pascal <pascal.baudel@yahoo.fr>
2024-08-02 10:02:10 +0200

    easyadmin

---
+++ src/Controller/Admin/PlatCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Plat;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PlatCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Plat::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
